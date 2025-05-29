d390dbc3f402261e4a9658e872c9b171bacd0d4d
Matthew Kearney <matney@clpc296.cs.ox.ac.uk>
2025-05-30 00:32:09 +0100

    chagen tictactoe moves to text

--- src/prompts/tictactoe.py
+++ src/prompts/tictactoe.py
@@ -1,34 +1,34 @@
 from src.prompts.prompt_utils import get_answer_format, extract_final_answer
 
 def create_prompt(game_state):
-    player_symbol_dict = {1: "X", 0: "-", 1: '0'}
-    board = board_to_str(game_state["board"])
+    player_symbol_dict = {-1: "X", 0: "-", 1: 'O'}
+    board = board_to_str(game_state["board"], player_symbol_dict)
     player_symbol = player_symbol_dict[game_state["next_player"]]
-    n_dim = len(game_state["board"])
-    dim_num_strs = ", ".join([x for x in range(1, n_dim)]) + f", and {n_dim}" 
+    n_dim = len(board)
+    # dim_num_strs = ", ".join([x for x in range(1, n_dim)]) + f", and {n_dim}" 
     
-    prompt =
-    f"""
-    We are playing tic-tac-toe. You are {player_symbol}. It is your move. The current board is:
+    prompt =f"""
+We are playing tic-tac-toe. You are {player_symbol}. It is your move. The current board is:
+    
+{board}
 
-    ```
-    {board}
-    ```
-
-    Think carefully about the optimal move and then choose the space to move in. Your final answer should be of the format "{get_answer_format("row, col")}" where row is the row number, which are numbered from top to bottom {dim_num_strs}, and col is the column number, which are numbered from left to right.{dim_num_strs}.
+Think carefully about the optimal move and then choose the space to move in. Your final answer should be of the format "{get_answer_format("row, col")}" where row is one of \{top, bottom, center\} and col is one of \{left, center, right\}.
     """
 
     return prompt
     
 def evaluate_response(game_state, model_response):
+    row_text_to_idx = {"top": 0, "center": 1, "bottom": 2}
+    col_text_to_idx = {"left": 0, "center": 1, "right": 2}
     try:
         answer = extract_final_answer(model_response)
-        answer = tuple([int(x.strip())+1 for x in answer.split(",")])
+        row, col = answer.lower().split(",")
+        move = (row_text_to_idx[row], col_text_to_idx[col])
     except:
         return None
 
-    move_to_outcome = {move["move"]: move["move_outcome"] for move in game_state["moves"]}
-    if not len(answer) == 2 or not move in move_to_outcome:
+    move_to_outcome = {m["move"]: m["move_outcome"] for m in game_state["moves"]}
+    if not move in move_to_outcome:
         return None
 
     best_move_outcome = max(move_to_outcome.values())
